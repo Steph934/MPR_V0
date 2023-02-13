1b8914c31588e121703306aedd5c256e3dc4c0a8
Steph934 <quinionstephane@gmail.com>
2023-02-13 11:18:51 +0100

    test_fix_error testPost

--- my-app/src/services/user/UserServices.js
+++ my-app/src/services/user/UserServices.js
@@ -4,33 +4,35 @@ import axios from 'axios';
 const apiMPR = axios.create({
     baseURL: 'http://localhost:8080',
     timeout: 10000,
-    Headers: {"Access-Control-Allow-Origin": "true"},
-    crossorigin:true
+    Headers: {
+        "Access-Control-Allow-Origin": "true"
+    },
+    crossorigin: true
 });
 
 // https://axios-http.com/fr/docs/instance <= config requet axios
 
 const config = {
     headers: {
-            'Content-Type': 'application/json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "*",
-            },
-        }
+        'Content-Type': 'application/json;charset=UTF-8',
+        "Access-Control-Allow-Origin": "*",
+    },
+}
 
 
 
 export const testPost = async (params) => {
     await axios.post('http://localhost:8080/signup', params, config)
-      this.then((response) => {
-         return response
-            .then((data) => {
-                console.log(data);
-            })
-    }, (error) => {
-        return error.response.data.message;
-      });
+        //   .then((response) => {
+        //     response.data.message
+        // }, (error) => {
+        //     error.response.data.message
+        //   });
+        .then((response) => console.log(response.data.message))
+        .catch((error) => console.log(error));
 
-} 
+
+}
 
 
 export const test = {
