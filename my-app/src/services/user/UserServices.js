8c7a94311fd75e0eba493a3e7868bf51646a9018
tonylapoche <terrade.antoine.pro@gmail.com>
2023-02-22 16:20:19 +0100

    mise a jour API request

--- my-app/src/services/user/UserServices.js
+++ my-app/src/services/user/UserServices.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 const apiMPR = axios.create({
     baseURL: 'http://localhost:8080',
     timeout: 10000,
-<<<<<<< HEAD
     Headers: {
         "Access-Control-Allow-Origin": "true"
     },
@@ -13,49 +12,31 @@ const apiMPR = axios.create({
 
 // https://axios-http.com/fr/docs/instance <= config requet axios
 
-const config = {
-    headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        "Access-Control-Allow-Origin": "*",
-    },
+// const config = {
+//     headers: {
+//         'Content-Type': 'application/json;charset=UTF-8',
+//         "Access-Control-Allow-Origin": "*",
+//     },
+// }
+
+
+
+export const signUp =  (params) => {
+    apiMPR.post('signup', params)
 }
 
 
+// export const test = {
+//     async signup(params) {
+//         try {
+//             // apiMPR.defaults.headers.common['Authorizations'] = '';
+//             const response = await apiMPR.post('/signup', params);
 
-export const testPost = async (params) => {
-    await axios.post('http://localhost:8080/signup', params, config)
-        //   .then((response) => {
-        //     response.data.message
-        // }, (error) => {
-        //     error.response.data.message
-        //   });
-        .then((response) => console.log(response.data.message))
-        .catch((error) => console.log(error));
+//             return console.log(response)
+//         } catch (error) {
+//             return error.response
+//         }
+//     }
+// }
 
-
-}
-
-
-export const test = {
-    async signup(params) {
-        try {
-            // apiMPR.defaults.headers.common['Authorizations'] = '';
-            const response = await apiMPR.post('/signup', params);
-
-            return console.log(response)
-        } catch (error) {
-            return error.response
-        }
-    }
-}
-
-export default test
-=======
-    Headers: {"Access-Control-Allow-Origin": "true"},
-});
-
-export const signUp = (params) => {
-    const myP =  apiMPR.post('http://localhost:8080/signup', params)
-    return myP
-} 
->>>>>>> 9e5b8bf8b15aafac64d8f513403f3a394310b183
+// export default test
